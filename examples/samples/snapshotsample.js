b5784169411d011ba5d1b8c795c2b80b6c8b838e
Maggie Ying <maggie.ying@gmail.com>
2012-11-20 12:23:18 -0800

    Add test for the snapshotsample and also cleanup sample

--- examples/samples/snapshotsample.js
+++ examples/samples/snapshotsample.js
@@ -26,7 +26,7 @@
 * 
 * 5. List blobs including snapshots.
 * 
-* 6. Promote the first snapshot.
+* 6. Read from the first snapshot.
 * 
 * 7. Delete the first snapshot.
 * 
@@ -51,61 +51,71 @@ var CloudBlobClient = azure.CloudBlobClient;
 
 var util = require('util');
 
-var container = 'snapshotsample';
-var blob = 'snapshotsample';
+var async = require('async');
 
-var blockId1 = 'b1';
-var blockId2 = 'b2';
-var blockId3 = 'b3';
+// Expose 'SnapshotSample'.
+function SnapshotSample() {}
 
-var blockContent1 = 'content1';
-var blockContent2 = 'content2';
-var blockContentAlternative2 = 'alternative2';
-var blockContent3 = 'content3';
+exports = module.exports = SnapshotSample;
+SnapshotSample.container = 'snapshotsample9994';
+SnapshotSample.blob = 'snapshotsample';
 
+SnapshotSample.blockId1 = 'b1';
+SnapshotSample.blockId2 = 'b2';
+SnapshotSample.blockId3 = 'b3';
+
+SnapshotSample.blockContent1 = 'content1';
+SnapshotSample.blockContent2 = 'content2';
+SnapshotSample.blockContentAlternative2 = 'alternative2';
+SnapshotSample.blockContent3 = 'content3';
+
+SnapshotSample.snapshot1 = null;
+
+var sample = new SnapshotSample();
 var blobClient = azure.createBlobService();
 
-function createContainer() {
-  // Step 0: Check if the target container exists.
-  blobClient.createContainerIfNotExists(container, function (error) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      console.log('Created the container ' + container);
-      uploadblockBlobs();
-    }
-  });
+SnapshotSample.prototype.CreateContainer = function (callback) {
+    // Step 0: Check if the target container exists.
+    console.log(SnapshotSample.container);
+    blobClient.createContainerIfNotExists(SnapshotSample.container, function (error) {
+        if (error) {
+            throw error;
+        }
+        else {
+            console.log('Created the container ' + SnapshotSample.container);
+            callback();
+        }
+    });
 }
 
-function uploadblockBlobs() {
+SnapshotSample.prototype.UploadBlockBlobs = function(callback) {
   // Step 1: Upload 3 blocks and commit them.
   var blockList = {
-    LatestBlocks: [blockId1, blockId2, blockId3]
+    LatestBlocks: [SnapshotSample.blockId1, SnapshotSample.blockId2, SnapshotSample.blockId3]
   };
 
-  blobClient.createBlobBlockFromText(blockList.LatestBlocks[0], container, blob, blockContent1, blockContent1.length, function (error1) {
+  blobClient.createBlobBlockFromText(blockList.LatestBlocks[0], SnapshotSample.container, SnapshotSample.blob, SnapshotSample.blockContent1, SnapshotSample.blockContent1.length, function (error1) {
     if (error1) {
-      console.log(error1);
+      throw error1;
     } else {
       console.log('Uploaded the block whose ID is ' + blockList.LatestBlocks[0]);
-      blobClient.createBlobBlockFromText(blockList.LatestBlocks[1], container, blob, blockContent2, blockContent2.length, function (error2) {
+      blobClient.createBlobBlockFromText(blockList.LatestBlocks[1], SnapshotSample.container, SnapshotSample.blob, SnapshotSample.blockContent2, SnapshotSample.blockContent2.length, function (error2) {
         if (error2) {
-          console.log(error2);
+          throw error2;
         } else {
           console.log('Uploaded the block whose ID is ' + blockList.LatestBlocks[1]);
-          blobClient.createBlobBlockFromText(blockList.LatestBlocks[2], container, blob, blockContent3, blockContent3.length, function (error3) {
+          blobClient.createBlobBlockFromText(blockList.LatestBlocks[2], SnapshotSample.container, SnapshotSample.blob, SnapshotSample.blockContent3, SnapshotSample.blockContent3.length, function (error3) {
             if (error3) {
-              console.log(error3);
+              throw error3;
             } else {
               console.log('Uploaded the block whose ID is ' + blockList.LatestBlocks[2]);
-              blobClient.commitBlobBlocks(container, blob, blockList, function (error4) {
+              blobClient.commitBlobBlocks(SnapshotSample.container, SnapshotSample.blob, blockList, function (error4) {
                 if (error4) {
-                  console.log(error4);
+                  throw error4;
                 }
                 else {
-                  console.log('Committed the blob ' + blob);
-                  createSnapshot();
+                  console.log('Committed the blob ' + SnapshotSample.blob);
+                  callback();
                 }
               });
             }
@@ -116,114 +126,135 @@ function uploadblockBlobs() {
   });
 }
 
-function createSnapshot() {
-  // Step 2 : Creates a snapshot.
-  blobClient.createBlobSnapshot(container, blob, function (error, snapshot1) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Created a snapshot for the blob ' + blob);
-
-      createBlob(snapshot1);
-    }
-  });
+SnapshotSample.prototype.ReadBlobContent = function (callback) {
+    blobClient.getBlobToText(SnapshotSample.container, SnapshotSample.blob, function (error, text) {
+        if (error) {
+            throw error;
+        }
+        else {
+            console.log('Reading the current blob content: ' + text);
+            callback(null, text);
+        }
+    })
 }
 
-function createBlob (snapshot) {
-  // Step 3: Update the block 2, commit the blob again.
-  blobClient.createBlobBlockFromText(blockId2, container, blob, blockContentAlternative2, blockContentAlternative2.length, function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Uploaded the block whose ID is ' + blockId2);
-
-      var blockList = {
-        LatestBlocks: [blockId1, blockId2, blockId3]
-      };
-
-      blobClient.commitBlobBlocks(container, blob, blockList, function (error2) {
-        if (error2) {
-          console.log(error2);
+SnapshotSample.prototype.CreateSnapshot = function (callback) {
+    // Step 2 : Creates a snapshot.
+    blobClient.createBlobSnapshot(SnapshotSample.container, SnapshotSample.blob, function (error, snapshot1) {
+        if (error) {
+            throw error;
         } else {
-          console.log('Committed the blob ' + blob);
-          createAnotherSnapshot(snapshot);
+            console.log('Created a snapshot for the blob ' + SnapshotSample.blob);
+            SnapshotSample.snapshot1 = snapshot1;
+            callback();
         }
-      });
+    });
+}
+
+SnapshotSample.prototype.UpdateBlock2 = function (callback) {
+    // Step 3: Update the block 2, commit the blob again.
+    blobClient.createBlobBlockFromText(SnapshotSample.blockId2, SnapshotSample.container, SnapshotSample.blob, SnapshotSample.blockContentAlternative2, SnapshotSample.blockContentAlternative2.length, function (error) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Uploaded the block whose ID is ' + SnapshotSample.blockId2);
+
+            var blockList = {
+                LatestBlocks: [SnapshotSample.blockId1, SnapshotSample.blockId2, SnapshotSample.blockId3]
+            };
+
+            blobClient.commitBlobBlocks(SnapshotSample.container, SnapshotSample.blob, blockList, function (error2) {
+                if (error2) {
+                    throw error2;
+                } else {
+                    console.log('Committed the blob ' + SnapshotSample.blob);
+                    callback();
+                }
+            });
+        }
+    });
+}
+SnapshotSample.prototype.CreateAnotherSnapshot = function (callback) {
+    // Step 4 : Creates another snapshot.
+    blobClient.createBlobSnapshot(SnapshotSample.container, SnapshotSample.blob, function (error) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Created a snapshot for the blob ' + SnapshotSample.blob);
+            callback();
+        }
+    });
+};
+
+SnapshotSample.prototype.ListSnapshots = function (callback) {
+    // Step 5 : List the blobs, including snapshots
+    blobClient.listBlobs(SnapshotSample.container, { include: 'snapshots' }, function (error, blobResults) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Listing the blobs under the container ' + SnapshotSample.container);
+
+            blobResults.forEach(function (blobResult) {
+                console.log('  Blob: ' + blobResult.url);
+            });
+
+            callback(null, blobResults);
+        }
+    });
+};
+
+SnapshotSample.prototype.ReadBlobContentFromSnapshot = function (callback) {
+    // Step 6 : Read from snapshot1.
+    blobClient.getBlobToText(SnapshotSample.container, SnapshotSample.blob, { snapshotId: SnapshotSample.snapshot1 }, function (error, text) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Reading from snapshot ' + SnapshotSample.snapshot1 + ': ' + text);
+            callback(null, text);
+        }
+    });
+}
+
+SnapshotSample.prototype.DeleteSnapshot = function (callback) {
+    // Step 7 : Delete the first snapshot.
+    blobClient.deleteBlob(SnapshotSample.container, SnapshotSample.blob, { snapshotId: SnapshotSample.snapshot1 }, function (error) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Deleted the snapshot ' + SnapshotSample.snapshot1);
+            callback();
+        }
+    });
+};
+
+SnapshotSample.prototype.ListOnlySnapshots = function (callback) {
+    // Step 8 : List the snapshots.
+    blobClient.listBlobs(SnapshotSample.container, { prefix: SnapshotSample.blob, include: 'snapshots' }, function (error, blobResults) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Listing snapshots for the blob ' + SnapshotSample.blob);
+
+            blobResults.forEach(function (blobResult) {
+                if (blobResult.snapshot) {
+                    console.log('  Snapshot: ' + blobResult.snapshot);
+                }
+            });
+
+            callback(null, blobResults);
+        }
+    });
+};
+
+SnapshotSample.prototype.DeleteContainer = function (callback) {
+    blobClient.deleteContainer(SnapshotSample.container, function (error) {
+    if (error) {
+      console.log(error);
+    } else {
+      callback();
     }
   });
 }
-
-function createAnotherSnapshot (snapshot) {
-  // Step 4 : Creates another snapshot.
-  blobClient.createBlobSnapshot(container, blob, function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Created a snapshot for the blob ' + blob);
-
-      listSnapshots(snapshot);
-    }
-  });
-};
-
-function listSnapshots (snapshotId) {
-  // Step 5 : List the blobs, including snapshots
-  blobClient.listBlobs(container, { include: 'snapshots' }, function (error, blobResults) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Listing the blobs under the container ' + container);
-
-      blobResults.forEach(function (blobResult) {
-        console.log('  Blob: ' + blobResult.url);
-      });
-
-      promoteSnapshot(snapshotId);
-    }
-  });
-};
-
-function promoteSnapshot (snapshot) {
-  // Step 6 : Promote, delete the snapshot1.
-  blobClient.createBlockBlobFromText(container, blob, 'promoted', { snapshotId: snapshot }, function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Promoted the snapshot ' + snapshot);
-      deleteSnapshot(snapshot);
-    }
-  });
-}
-
-function deleteSnapshot (snapshot) {
-  // Step 7 : Delete the first snapshot.
-  blobClient.deleteBlob(container, blob, { snapshotId: snapshot }, function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Deleted the snapshot ' + snapshot);
-
-      listOnlySnapshots();
-    }
-  });
-};
-
-function listOnlySnapshots () {
-  // Step 8 : List the snapshots.
-  blobClient.listBlobs(container, { prefix: blob, include: 'snapshots' }, function (error, blobResults) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Listing snapshots for the blob ' + blob);
-
-      blobResults.forEach(function (blobResult) {
-        if (blobResult.snapshot) {
-          console.log('  Snapshot: ' + blobResult.snapshot);
-        }
-      });
-    }
-  });
-};
 
 var arguments = process.argv;
 
@@ -232,14 +263,43 @@ if (arguments.length > 3) {
 }
 else if (arguments.length == 3) {
   // Adding a third argument on the command line, whatever it is, will delete the container before running the sample.
-  blobClient.deleteContainer(container, function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      createContainer();
-    }
-  });
+sample.DeleteContainer(function (error) {
+        if (error) {
+            console.log(error);
+        } else {
+            startSample();
+        } 
+    });
 }
 else {
-  createContainer();
+  startSample();
+}
+
+function startSample() {
+
+    async.series([
+        // 1. Upload 3 blocks and commit them.
+        sample.CreateContainer,
+        sample.UploadBlockBlobs,
+        sample.ReadBlobContent,
+
+        // 2. Take a snapshot for that blob.
+        // 3. Re-upload one of the three blocks and commit them.
+        sample.CreateSnapshot,
+        sample.UpdateBlock2,
+        sample.ReadBlobContent,
+
+        // 4. Take a snapshot again.
+        // 5. List blobs including snapshots.
+        sample.CreateAnotherSnapshot,
+        sample.ListSnapshots,
+
+        // 6. Read from the first snapshot.
+        sample.ReadBlobContentFromSnapshot,
+
+        // 7. Delete the first snapshot.
+        // 8. List all snapshots for this blob.
+        sample.DeleteSnapshot,
+        sample.ListOnlySnapshots
+     ]);    
 }
--- test/endtoend/snapshotsample-test.js
+++ test/endtoend/snapshotsample-test.js
@@ -13,3 +13,70 @@
 * limitations under the License.
 */
 
+// Lib includes
+var assert = require('assert');
+var async = require('async');
+var snapshotsample = require('../../examples/samples/snapshotsample');
+var Sample = new snapshotsample();
+
+suite('snapshotsample-test', function () {
+    setup(function (done) {
+        done();
+    });
+
+    teardown(function (done) {
+        Sample.DeleteContainer(function () {
+            done();
+        });
+    });
+
+    test('Basic', function (done) {
+
+        this.timeout(1000000);
+        async.series([
+            // 1. Upload 3 blocks and commit them.
+            Sample.CreateContainer,
+            Sample.UploadBlockBlobs,
+            Sample.ReadBlobContent,
+
+            // 2. Take a snapshot for that blob.
+            // 3. Re-upload one of the three blocks and commit them.
+            Sample.CreateSnapshot,
+            Sample.UpdateBlock2,
+            Sample.ReadBlobContent,
+
+            // 4. Take a snapshot again.
+            // 5. List blobs including snapshots.
+            Sample.CreateAnotherSnapshot,
+            Sample.ListSnapshots,
+
+            // 6. Read from the first snapshot.
+            Sample.ReadBlobContentFromSnapshot,
+
+            // 7. Delete the first snapshot.
+            // 8. List all snapshots for this blob.
+            Sample.DeleteSnapshot,
+            Sample.ListOnlySnapshots,
+        ], function (error, results) {
+            // From #1 - Validate block content
+            assert.equal(results[2], snapshotsample.blockContent1 +
+                snapshotsample.blockContent2 + snapshotsample.blockContent3);
+
+            // From #2-3 - Validate updated block content
+            assert.equal(results[5], snapshotsample.blockContent1 +
+                snapshotsample.blockContentAlternative2 + snapshotsample.blockContent3);
+
+            // From #4-5 - Validate number of snapshots
+            assert.equal(results[7].length, 3);
+
+            // From #6 - Validate reading block from snapshot
+            assert.equal(results[8], snapshotsample.blockContent1 +
+                snapshotsample.blockContent2 + snapshotsample.blockContent3);
+
+            // From #7-8 - Validate number of snapshots
+            assert.equal(results[10].length, 2);
+
+            done();
+        });
+    });
+});
